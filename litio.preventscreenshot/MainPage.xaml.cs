047ba439e8932e40b8b66c4012191d1c4075aa47
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2022-09-05 16:44:13 -0400

    modified navigation button text and clarify title text

--- litio.preventscreenshot/MainPage.xaml.cs
+++ litio.preventscreenshot/MainPage.xaml.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace litio.preventscreenshot
@@ -13,6 +8,11 @@ namespace litio.preventscreenshot
     public MainPage()
     {
       InitializeComponent();
+    }
+
+    private void GoToScreenshotSafePage(object sender, EventArgs e)
+    {
+      this.Navigation.PushAsync(new ArticlesPage());
     }
   }
 }
